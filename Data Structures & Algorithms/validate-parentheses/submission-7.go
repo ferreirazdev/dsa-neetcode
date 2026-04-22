bdb36ec521b953b28cda7609a979c4577dde2be2
Flávio Ferreira <business.flavioferreira@gmail.com>
2026-04-22 06:37:34 -0300

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.go
@@ -0,0 +1,23 @@
+func isValid(s string) bool {
+    stack := []rune{}
+	closeToOpen := map[rune]rune{')':'(', '}':'{', ']':'['}
+
+	for _, c := range s {
+		if open, exists := closeToOpen[c]; exists {
+			if len(stack) == 0 {
+				return false
+			}
+
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			if top != open {
+				return false
+			}
+		} else {
+			stack = append(stack, c)
+		}
+	}
+
+	return len(stack) == 0
+}
